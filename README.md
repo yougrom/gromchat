51edd2a82288b34701ed3ab375b2aedf84c1490c
Herr Gromov <youriy.gromov@gmail.com>
2024-05-03 14:25:49 +0200

    feat: Add custom ActionSheet with image, photo, and location sharing capabilities

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
+# Grom Chat: Mobile Chat App
 ## Project description
 
-## Project Features
+- **Instant Communication:** Effortlessly chat, share images, and update your location with just a few taps.
+- **Seamless Connectivity:** Stay connected with friends and family, whether online or offline, ensuring you never miss a moment.
+- **Accessibility:** Our inclusive design supports screen readers, making it accessible for visually impaired users.
+
+---
+
+## Tech Stack Overview
+
+- **React Native:** Core framework for building the app.
+- **Expo:** Utilized for streamlined development processes.
+- **Stylish Design:** Matches specific screen design aesthetics.
+- **Google Firestore Database:** Stores chat conversations securely.
+- **Google Firebase Authentication:** Provides anonymous user authentication.
+- **Local Storage:** Enables offline access to chat conversations.
+- **Image Sharing:** Supports uploading images from the library or capturing new ones.
+- **Firebase Cloud Storage:** Securely stores shared images.
+- **Location Data:** Integrates location sharing within chat through a map view.
+- **Gifted Chat Library:** Powers the chat interface and functionalities.
+- **Well-Documented:** Codebase is thoroughly commented for clarity.
+
+---
+
+## Setup Instructions
+
+Follow these steps to get the app running on your local machine:
+
+1. **Clone the repository:** Use the command `git clone https://github.com/yougrom/gromchat.git` to clone the repository to your local machine.
+
+2. **Navigate to the project directory:** Use the command `cd your-repo-name` to navigate into the root directory of the project. Remember to replace `your-repo-name` with the actual name of your repository.
+
+3. **Install Node.js and npm:** If you don't have Node.js and npm installed, download them from [here](https://nodejs.org/).
+
+4. **Install Expo CLI:** Use the command `npm install -g expo-cli` to install Expo CLI globally on your machine.
+
+5. **Install dependencies:** Use the command `npm install` to install all the necessary dependencies for the project.
+
+6. **Start the app:** Use the command `npm start` to start the app. This will open a new browser window with the Expo developer tools.
+
+7. **Run the app on your device/emulator:** You can either use an Android/iOS emulator or directly run the app on your mobile device using the Expo Go app. Scan the QR code displayed in the Expo developer tools with the Expo Go app to run the app on your device.
+
+   Please note that you'll need to have the Expo Go app installed on your device to run the app. You can download it from the [App Store](https://apps.apple.com/app/expo-go/id982107779) or [Google Play Store](https://play.google.com/store/apps/details?id=host.exp.exponent&referrer=www).
+
+
+![Chat Screen](./assets/img.jpg)
--- components/Chat.js
+++ components/Chat.js
@@ -1,133 +1,170 @@
-import React, { useState, useEffect, useLayoutEffect } from "react";
-import { StyleSheet, Platform, KeyboardAvoidingView } from "react-native";
-import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useState, useEffect, useCallback } from "react";
 
-import AsyncStorage from "@react-native-async-storage/async-storage";
+// import components from React Native
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from "react-native";
 
+//import the Gifted Chat library
+import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import {
   collection,
   addDoc,
+  onSnapshot,
   query,
   orderBy,
-  onSnapshot,
 } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import MapView from "react-native-maps";
+import CustomActions from "./CustomActions";
 
 // Define the Chat component with props received from navigation
-const Chat = ({ route, navigation, db, isConnected }) => {
-  const { name, backgroundColor, _id } = route.params;
+const Chat = ({ db, route, navigation, isConnected, storage }) => {
   const [messages, setMessages] = useState([]);
+  const { name, backgroundColor, _id } = route.params;
 
-  // Call this function if the isConnected prop turns out to be false in useEffect()
-  const loadCachedMessages = async () => {
-    try {
-      const cachedMessages = await AsyncStorage.getItem("messages");
-      setMessages(cachedMessages ? JSON.parse(cachedMessages) : []);
-    } catch (error) {
-      console.error("Failed to load or parse cached messages:", error);
-      setMessages([]);
-    }
-  };
+  let unsubMessages;
 
-  // Function to handle sending new messages and save them to Firestore
-  const onSend = async (newMessages = []) => {
-    const messageToSend = {
-      ...newMessages[0],
-      user: {
-        _id: _id || 1, // Установити типове значення, якщо _id не передається
-        name: name, // Встановлюємо ім'я відправника
-      },
+  useEffect(() => {
+    navigation.setOptions({ title: name });
+
+    if (isConnected === true) {
+      if (unsubMessages) unsubMessages();
+      unsubMessages = null;
+
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+      unsubMessages = onSnapshot(q, (docs) => {
+        let newMessages = [];
+        docs.forEach((doc) => {
+          newMessages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis()),
+          });
+        });
+        cacheMessages(newMessages);
+        setMessages(newMessages);
+      });
+    } else loadCachedMessages();
+
+    return () => {
+      if (unsubMessages) unsubMessages();
     };
+  }, [isConnected]);
 
+  const loadCachedMessages = async () => {
+    const cachedMessages = (await AsyncStorage.getItem("messages")) || [];
+    setMessages(JSON.parse(cachedMessages));
+  };
+
+  const cacheMessages = async (messagesToCache) => {
     try {
-      await addDoc(collection(db, "messages"), messageToSend);
+      await AsyncStorage.setItem("messages", JSON.stringify(messagesToCache));
     } catch (error) {
-      console.error("Error adding document: ", error);
+      console.log(error.message);
     }
   };
+
+  const onSend = useCallback(
+    async (newMessages = []) => {
+      const messageToSend = {
+        ...newMessages[0],
+        user: {
+          _id: _id || "default_id",
+          name: name,
+        },
+      };
+
+      try {
+        await addDoc(collection(db, "messages"), messageToSend);
+      } catch (error) {
+        console.error("Error adding document:", error);
+      }
+    },
+    [_id, name, db]
+  );
 
   const renderInputToolbar = (props) => {
-    if (isConnected) return <InputToolbar {...props} />;
+    if (isConnected === true) return <InputToolbar {...props} />;
     else return null;
   };
 
-  // Customize the chat bubbles' styling
-  const renderBubble = (props) => (
-    <Bubble
-      {...props}
-      wrapperStyle={{
-        right: {
-          backgroundColor: "#000",
-        },
-        left: {
-          backgroundColor: "#FFF",
-        },
-      }}
-    />
-  );
-  let unsubMessages;
-  // Initialize messages state when component mounts
-  useEffect(() => {
-    let unsubscribe = () => {}; // Default to a no-op function
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#000",
+          },
+          left: {
+            backgroundColor: "#FFF",
+          },
+        }}
+      />
+    );
+  };
 
-    if (isConnected) {
-      const messagesRef = collection(db, "messages");
-      const q = query(messagesRef, orderBy("createdAt", "desc"));
-      unsubscribe = onSnapshot(q, (snapshot) => {
-        const fetchedMessages = snapshot.docs.map((doc) => ({
-          _id: doc.id,
-          text: doc.data().text,
-          createdAt: doc.data().createdAt.toDate(),
-          user: doc.data().user,
-        }));
-        setMessages(fetchedMessages);
-        AsyncStorage.setItem("messages", JSON.stringify(fetchedMessages));
-      });
-    } else {
-      loadCachedMessages();
-    }
+  //create function for the custom componet "+" btn
+  const renderCustomActions = (props) => {
+    return <CustomActions storage={storage} {...props} />;
+  };
 
-    return () => unsubscribe(); // Clean up the subscription
-  }, [isConnected, db]);
-
-  // Update navigation options dynamically
-  useEffect(() => {
-    navigation.setOptions({ title: name });
-  }, []);
-
-  // Set the navigation bar title on the loading screen
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      title: name || "Chat",
-    });
-  }, [name, navigation]);
-
-  // Render the GiftedChat component
-  return (
-    <SafeAreaView style={[styles.container, { backgroundColor }]}>
-      <KeyboardAvoidingView
-        style={{ flex: 1 }}
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        keyboardVerticalOffset={Platform.OS === "ios" ? -200 : 0}
-      >
-        <GiftedChat
-          messages={messages}
-          renderBubble={renderBubble}
-          onSend={onSend} // Updated to send messages to Firestore
-          user={{
-            _id: _id, // Установити типове значення, якщо _id не передається
-            name: name,
+  //create function for custom component to render location data
+  const renderCustomView = (props) => {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
           }}
         />
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+      );
+    }
+    return null;
+  };
+  // Render the GiftedChat component
+  return (
+    <View style={[styles.container, { backgroundColor }]}>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar}
+        onSend={(messages) => onSend(messages)}
+        renderActions={renderCustomActions}
+        renderCustomView={renderCustomView}
+        user={{
+          _id: _id,
+          name,
+        }}
+      />
+
+      {Platform.OS === "android" ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
+    </View>
   );
 };
 
-// StyleSheet for the container
+// Create style
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  logoutButton: {
+    position: "absolute",
+    right: 0,
+    top: 0,
+    backgroundColor: "#C00",
+    padding: 10,
+    zIndex: 1,
+  },
+  logoutButtonText: {
+    color: "#FFF",
+    fontSize: 10,
   },
 });
 
